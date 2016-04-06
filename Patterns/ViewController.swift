f7284bb3b630960c35b9b135711486f621b90bad
Gerry Laplante <GerryLaplante@gmail.com>
2016-04-06 16:19:24 -0400

    wire up tableView

--- Patterns/ViewController.swift
+++ Patterns/ViewController.swift
@@ -2,24 +2,31 @@
 //  ViewController.swift
 //  Patterns
 //
-//  Created by Paul Wright on 4/6/16.
+//  Created by Gerry Laplante on 4/6/16.
 //  Copyright © 2016 Gerry Laplante. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tvPatterns: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tvPatterns.dataSource = self
+        self.tvPatterns.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel?.text = "No Shirts, No Shoes, No Service"
+        return cell
     }
 
 
