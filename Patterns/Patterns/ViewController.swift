64154a879ec09afa22bf5f049cb7eb7d6e8ac082
Gerry Laplante <GerryLaplante@gmail.com>
2016-04-06 20:05:28 -0400

    add tableview

--- Patterns/Patterns/ViewController.swift
+++ Patterns/Patterns/ViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tblViewPatterns: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tblViewPatterns.dataSource = self
+        self.tblViewPatterns.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel?.text = "No Shoes, No Shirt, No Problem"
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
